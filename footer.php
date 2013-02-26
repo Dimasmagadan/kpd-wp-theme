8a6ccc83400916bf7093891338a16b5466cfc46a
Dimas <dimasmagadan@gmail.com>
2013-02-26 16:52:46 +0400

    поправил перевод, добавил sape в футере

--- footer.php
+++ footer.php
@@ -5,26 +5,22 @@
  */
 ?>
 <footer>
+	<?php if ( function_exists('dynamic_sidebar') ) { dynamic_sidebar('bottom'); } else { ?>
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by
 		<a href="http://wordpress.org/">WordPress</a>, and built using the <a href="http://html5boilerplate.com/">HTML5 Boilerplate</a>.
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
 		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 	</p>
+	<?php } ?>
+	<?php
+		$kpd_options=get_option( 'kpd_options');
+		if(!empty($kpd_options['kpd_sape_code'])){
+			echo '<div class="sape">'.$kpd_options['kpd_sape_code'].'</div>';
+		}
+	?>
 </footer>
 </div>
-
-<?php
-if(is_user_logged_in() && !$user_id=='1'){
-	?><!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
-</script><?php
-}
-?>
 
 <?php wp_footer(); ?>
 
