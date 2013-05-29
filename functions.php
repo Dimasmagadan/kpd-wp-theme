678f3a60bcda33673bbf6e8a6fae1f838b27e32b
Dimas <dimasmagadan@gmail.com>
2013-05-29 18:55:34 +0400

    disable comments for attachments

--- functions.php
+++ functions.php
@@ -427,6 +427,15 @@ function check_referrer() {
 }
 add_action('check_comment_flood', 'check_referrer');
 
+/* disable comments for attachments */
+function filter_media_comment_status( $open, $post_id ) {
+	$post = get_post( $post_id );
+	if( $post->post_type == 'attachment' ) {
+		return false;
+	}
+	return $open;
+}
+add_filter( 'comments_open', 'filter_media_comment_status', 10, 2 );
 
 
 /*
