5314a5c1f10aff815f43f443a662fbc810d68d35
Dimas <dimasmagadan@gmail.com>
2013-06-07 23:05:11 +0400

    вывод js и css плагинов на нужных страница�
    скрипты и стили для контактной формы выводятся только на 15й странице

--- functions.php
+++ functions.php
@@ -168,7 +168,21 @@ register_nav_menus( array(
 	'footer' => __( 'Нижнее меню', 'footer' ),
 ) );
 
+/* отключаем стили и скрипты плагинов для остальных страниц */
+function deregister_cf7_javascript() {
+    if ( !is_page(15) ) { // проверить номер страницы
+        wp_deregister_script( 'contact-form-7' );
+    }
+}
+add_action( 'wp_print_scripts', 'deregister_cf7_javascript', 15 );
+function deregister_cf7_styles() {
+    if ( !is_page(15) ) { // проверить номер страницы
+        wp_deregister_style( 'contact-form-7' );
+    }
+}
+add_action( 'wp_print_styles', 'deregister_cf7_styles', 15 );
 
+/* подключаем скрипты и стили */
 function os_styles_and_scripts() {
 	// заменить на версию темы перед запуском
 	$ver=time(); //dev!!!
