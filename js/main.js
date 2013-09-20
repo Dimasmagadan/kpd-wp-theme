a9a1670ac8c96aa2fe52502002a29a9a99b19a42
Dimas <dimasmagadan@gmail.com>
2013-09-20 22:53:08 +0400

    updated magnific popup
    Updated magnific popup version and added Russian language

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 jQuery(document).ready(function(e) {
 
+	// jQuery('a[rel="lightbox"]').magnificPopup({
+	// 	gallery: {
+	// 		enabled: true
+	// 	},
+	// 	type: 'image'
+	// });
+
 	jQuery('a[rel="lightbox"]').magnificPopup({
+		tClose: 'Закрыть (Esc)',
+		tLoading: 'Загрузка...',
+		image: {
+			tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
+		},
+		ajax: {
+			tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
+		},
 		gallery: {
+			tPrev: 'Туда (влево)',
+			tNext: 'Сюда (вправо)',
+			tCounter: '%curr% из %total%',
 			enabled: true
 		},
-		type: 'image'
+		zoom: {
+			enabled: true,
+			duration: 300
+		},
+		type: 'image',
+		callbacks: {
+			buildControls: function() {
+				this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+			}
+		}
 	});
 
 });
