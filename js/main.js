94abe552076a8d7816ba61828a3d9704efac51e0
Dimas <dimasmagadan@gmail.com>
2013-11-19 13:13:25 +0400

    changed jQuery to use strict

--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
-jQuery(document).ready(function($){
+(function( $ ) {
+	"use strict";
+ 
+	$(function() {
+		$(document).ready(function(){
 
-	// $('a[rel="lightbox"]').magnificPopup({
-	// 	gallery: {
-	// 		enabled: true
-	// 	},
-	// 	type: 'image'
-	// });
+			//$('a[rel="lightbox"]').magnificPopup({
+			//	gallery: {
+			//		enabled: true
+			//	},
+			//	type: 'image'
+			//});
 
-	$('a[rel="lightbox"]').magnificPopup({
-		tClose: 'Закрыть (Esc)',
-		tLoading: 'Загрузка...',
-		image: {
-			tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
-		},
-		ajax: {
-			tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
-		},
-		gallery: {
-			tPrev: 'Туда (влево)',
-			tNext: 'Сюда (вправо)',
-			tCounter: '%curr% из %total%',
-			enabled: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300
-		},
-		type: 'image',
-		callbacks: {
-			buildControls: function() {
-				this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
-			}
-		}
+			$('a[rel="lightbox"]').magnificPopup({
+				tClose: 'Закрыть (Esc)',
+				tLoading: 'Загрузка...',
+				image: {
+					tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
+				},
+				ajax: {
+					tError: '<a href="%url%">ссылку</a> загрузить не вышло.'
+				},
+				gallery: {
+					tPrev: 'Туда (влево)',
+					tNext: 'Сюда (вправо)',
+					tCounter: '%curr% из %total%',
+					enabled: true
+				},
+				zoom: {
+					enabled: true,
+					duration: 300
+				},
+				type: 'image',
+				callbacks: {
+					buildControls: function() {
+						this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+					}
+				}
+			});
+
+		});
 	});
-
-});
+ 
+}(jQuery));
