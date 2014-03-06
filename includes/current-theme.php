baac4dc2b1312c31e9e9b91eeb75c32cb6592924
Dimas <dimasmagadan@gmail.com>
2014-03-06 17:04:42 +0400

    Ajax dummy function
    dummy function code for an ajax request

--- includes/current-theme.php
+++ includes/current-theme.php
@@ -4,6 +4,12 @@ function os_return_time( $seconds=7200 ) {
 	return $seconds;
 }
 
+/* echo error for nopriv ajax request */
+function os_nopriv(){
+	$out['status']='error';
+	echo json_encode($out);
+	exit;
+}
 
 /* отключаем стили и скрипты плагинов для остальных страниц */
 function deregister_cf7_javascript() {
@@ -19,4 +25,18 @@ function deregister_cf7_styles() {
 }
 // add_action( 'wp_print_styles', 'deregister_cf7_styles', 15 );
 
+
+/* ajax dummy code */
+/*
+function os_ (){
+	$out['status']='error';
+
+
+	echo json_encode($out);
+	exit;
+}
+add_action( 'wp_ajax_nopriv_', 'os_nopriv' );
+add_action( 'wp_ajax_nopriv_', '' );
+add_action( 'wp_ajax_', '' );
+*/
 
