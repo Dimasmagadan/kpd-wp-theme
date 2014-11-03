1276331c523c21e123bf363385b89cd81fcda240
Dimasmagadan <dimasmagadan@gmail.com>
2014-11-03 15:36:35 +0300

    Updated debug functions

--- includes/debug.php
+++ includes/debug.php
@@ -1,56 +1,26 @@
 <?php
 /**
 *
-* Debug and perfomance
+* Debug and helper functions
 *
 **/
-
-error_reporting(E_ALL); // dev
+if(WP_DEBUG){
+    error_reporting(E_ALL);
+}
 
 // Output which theme template file a post/page is using in the header
 function show_template() {
-    global $template;
-    print_r($template);
+    if(WP_DEBUG){
+        global $template;
+        print_r($template);
+    }
 }
 add_action('wp_head', 'show_template');
 
-
-/*
-Usage:
-    $frag = new CWS_Fragment_Cache( 'unique-key', 3600 ); // Second param is TTL
-    if ( !$frag->output() ) { // NOTE, testing for a return of false
-        functions_that_do_stuff_live();
-        these_should_echo();
-        // IMPORTANT
-        $frag->store();
-        // YOU CANNOT FORGET THIS. If you do, the site will break.
-    }
-*/
-class CWS_Fragment_Cache {
-    const GROUP = 'cws-fragments';
-    var $key;
-    var $ttl;
-
-    public function __construct( $key, $ttl ) {
-        $this->key = $key;
-        $this->ttl = $ttl;
-    }
-
-    public function output() {
-        $output = wp_cache_get( $this->key, self::GROUP );
-        if ( !empty( $output ) ) {
-            // It was in the cache
-            echo $output;
-            return true;
-        } else {
-            ob_start();
-            return false;
-        }
-    }
-
-    public function store() {
-        $output = ob_get_flush(); // Flushes the buffers
-        wp_cache_add( $this->key, $output, self::GROUP, $this->ttl );
+function de($a, $force = false){
+    if(WP_DEBUG || $force){
+        echo '<pre>';
+        var_dump($a);
+        echo '</pre>';
     }
 }
-
