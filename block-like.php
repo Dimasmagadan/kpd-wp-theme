6b100e19a98777b8dc67f51764143842cca02f37
Dimasmagadan <dimasmagadan@gmail.com>
2015-03-17 17:50:13 +0300

    поправил вывод атрибутов

--- block-like.php
+++ block-like.php
@@ -2,10 +2,10 @@
 	data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,gplus"
 	data-yashareTheme="counter"
 	data-yashareLink="<?php the_permalink(); ?>"
-	data-yashareTitle="<?php the_title(); ?>"
-	data-yashareDescription="<?php the_excerpt(); ?>"
+	data-yashareTitle="<?php the_title_attribute(); ?>"
+	data-yashareDescription="<?php esc_attr( get_the_excerpt() ); ?>"
 	<?php if(has_post_thumbnail( )){
-		$thumb=wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium');
+		$thumb=wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium');
 		?> data-yashareImage="<?php echo $thumb[0]; ?>"<?php
 	} ?>
 ></div>
